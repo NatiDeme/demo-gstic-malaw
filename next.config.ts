afd2769cec973191a9e83ad133678a11b0d25c35
NatiDeme <demelashnatnael@gmail.com>
2024-12-10 23:01:16 +0100

    add starting point to optimize image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+        pathname: '/a/idrxejgv3w/**'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Home() {
   return (
-    <div className='bg-maincolor h-80 w-80 rounded-lg'>
+    <div className='bg-maincolor h-[800px] rounded-lg'>
+      <Image
+        src="https://utfs.io/a/idrxejgv3w/3YsXihUpZdorxuJmsb25LDiHuAOM871mfBqlahgENJ0eQTpn"
+        width={500}
+        height={500}
+        alt="Picture of the author"
+    />
     </div>
   )
 }
